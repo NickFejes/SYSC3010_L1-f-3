bbdef1ee4347ec630b8b54e9aafe752f2f330905
NickFejes <nickfejes@cmail.carleton.ca>
2020-10-16 15:13:48 -0400

    Inital Commit for lab 5

---
+++ Lab-5/A.py
@@ -0,0 +1,4 @@
+# Module created by Nick Fejes for Lab 5
+
+def function_A():
+	print("A")
--- Lab-5/AB.py
+++ Lab-5/AB.py
@@ -1,8 +1,10 @@
 # SYSC3010 Lab 5 - AB.py
 #	The file to be jointly edited by Team Members A and B
 
+from A import * # Line added by Nick Fejes
 from B import * # Line added by Will Brooks
 
 def function_AB():
 	print("AB")
+	function_A()  # Line added by Nick Fejes
 	function_B() # Line added by Will Brooks
