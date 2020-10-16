fde6521343d196c2f39322211242dc6dec4ee5b8
Will Brooks <willrbrooks@cmail.carleton.ca>
2020-10-16 14:08:46 -0400

    Will Brooks Feat: Made changes required by Lab 5

--- Lab-5/AB.py
+++ Lab-5/AB.py
@@ -1,5 +1,8 @@
 # SYSC3010 Lab 5 - AB.py
 #	The file to be jointly edited by Team Members A and B
 
+from B import * # Line added by Will Brooks
+
 def function_AB():
 	print("AB")
+	function_B() # Line added by Will Brooks
