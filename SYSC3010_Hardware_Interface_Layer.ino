0700b4c82c0d01f911e71c02c9084d99c5a7359e
WillrBrooks <71397373+WillrBrooks@users.noreply.github.com>
2020-11-18 13:41:52 -0500

    Update SYSC3010_Hardware_Interface_Layer.ino
    Will Brooks refactor: Modified comments no significan functional changes

--- SYSC3010_Hardware_Interface_Layer.ino
+++ SYSC3010_Hardware_Interface_Layer.ino
@@ -2,105 +2,120 @@
 #include <Servo.h>
 
 
-//PWM
-#define SERVOPIN 9// PWM pin to control servo
-//Analog
-#define SERVOFEEDBACKPIN A1// optional feedback pin for the servo to double check the PWM
-//Analog
-#define PHOTORESISTORPIN A0// pin to read the photoresistor's value
-//Analog or digital TBD
-#define PIRPIN 4// pin to read the PIR sensor's output value
-
-/*
- * PIR SENSOR:
- * 
- * Neuftech 3X HC-SR501
- * 
- */
-
-//constants
-#define BAGPROXIMITYTHRESHOLD 670
-//servo max position
-#define SERVOHIGH 70
-//servo min position
-#define SERVOLOW 5
-
-
-// command sent from pi;
-int command;
-//temporary value for readings to be read into
-int val;
-//the motion sensor has been triggered
-bool motionIsTriggered;
-
-
-Servo dispensingServo;
-
-void setup() {
-
-  //set up the serial interface to communicate with the pi
-  Serial.begin(9600);
-
-  //set up all pins to be used
-  //pinMode(SERVOPIN, OUTPUT);
-  //pinMode(SERVOFEEDBACKPIN, INPUT);// not strickly needed but would be a good sanity check or can be used to manually impliment PWM
-  
-  //pinMode(PHOTORESISTORPIN, INPUT);
-  
-  
-  pinMode(PIRPIN, INPUT);
-
-  //servo should start in a known position
-  //analogWrite(SERVOPIN, 0);
-
-  dispensingServo.attach(9);
-  dispensingServo.write(SERVOLOW);
-
-  command = 0;
-  val = 0;
-
-
-  //setup the onboard LED to output for testing
-  pinMode(13, OUTPUT);
-  digitalWrite(13, LOW);
-}
-
-/*
+/*----------------------------------------------------------------------------
  * 
  * Command list:
  * 
- * 0  nop : VOID
+ * 0  NOP : VOID
  * 1  Check Motion Triggered : BOOLEAN
  * 2  READ BAG PRESENT  : BOOLEAN
  * 3  DISPENSE CANDY  : VOID
  * 4  Clear Motion Triggered  : VOID
  * 5  NOP : VOID
  * 6  NOP : VOID
- * 7  ACK : 7 //the ACK command can be used to confirm that the hardware was connected to successfully via UART
+ * 7  ACK : 7 //the ACK command can be used to confirm that the hardware is connected and is communicating properly via UART
+ * 
+ ----------------------------------------------------------------------------*/
+
+ #define CHECK_MOTION 1 
+ #define READ_BAG_PRESENT 2
+ #define DISPENSE_CANDY 3
+ #define CLEAR_MOTION 4
+ #define ACK 7
+
+
+/*
+ * PIR SENSOR Specs:
+ * 
+ * Neuftech 3X HC-SR501
+ * 
+ * Datasheet can be found at https://www.mpja.com/download/31227sc.pdf
  * 
  */
 
+
+/*----------------------------------------------------------------------------
+ * 
+ * Hardware Constants
+ * 
+ * This section has the constants that will be used within functions
+ * 
+ ---------------------------------------------------------------------------*/
+
+// pin on the arduino to control servo.  Must be PWM capable
+#define SERVOPIN 9
+
+// pin on the arduino to read the photoresistor's value
+#define PHOTORESISTORPIN A0
+
+// pin on the arduino to read the PIR sensor's output value
+#define PIRPIN 4
+
+
+
+/*----------------------------------------------------------------------------
+ * 
+ * Software Constants
+ * 
+ * This section has the constants that will be used within functions
+ * 
+ ---------------------------------------------------------------------------*/
+
+//The analog value read from the voltage divider with the photoresistor above which a bag is considered to be present
+#define BAGPROXIMITYTHRESHOLD 200
+
+//max angle of the servo when dispensing candy
+#define SERVOHIGH 70
+
+//max angle of the servo when dispensing candy.  This is also the steady state angle of the servo
+#define SERVOLOW 5
+
+
+// command sent from pi;
+int command;
+
+//temporary value for storing sensor values
+int val;
+
+//the trigger state of motion sensor has been triggered a 1 value means that motion has been detected a 0 value means that no motion has been detected
+bool motionIsTriggered;
+
+//declare a servo object.  Must be global to be properly use Arduino's library.
+Servo dispensingServo;
+
+void setup() {
+
+  //set up the serial interface to communicate with the pi
+  Serial.begin(9600);
+  
+  pinMode(PIRPIN, INPUT);
+
+  //initialize the servo and set it to the steady state location
+  dispensingServo.attach(9);
+  dispensingServo.write(SERVOLOW);
+
+  command = 0;
+  val = 0;
+
+}
+
+
+//any time a UART event is detected this function is called
 void serialEvent(){
   if (Serial.available() > 0) {
-    //command = Serial.read();
+    
     command = Serial.parseInt();
-    //Serial.println(command);
+    
   }
   
 }
  
 void loop() {
-  /*if (Serial.available() > 0) {
-
-    command = Serial.parseInt();
-    //Serial.println(command);
-    
-   }*/
-
+  
   
   switch(command){
-    case (1):
-      //Check Motion Triggered : BOOLEAN
+    case (CHECK_MOTION):
+      
       if(motionIsTriggered){
         Serial.println("1");
         Serial.flush();
@@ -111,53 +126,53 @@ void loop() {
       }
       
     break;
-    case (2):
-      //READ BAG PRESENT  : BOOLEAN
+    case (READ_BAG_PRESENT):
       
       val = analogRead(PHOTORESISTORPIN);
       if(val <= BAGPROXIMITYTHRESHOLD){
-        //Serial.println(val);
+        
         Serial.println("1");
         Serial.flush();
       }
       else{
-        //Serial.println(val);
+        
         Serial.println("0");
         Serial.flush();
       }
       
     break;
-    case (3):
-      //3  DISPENSE CANDY  : VOID
+    case (DISPENSE_CANDY):
+
+      //command servo to release a piece of candy
       dispensingServo.write(SERVOLOW);
       delay(500);
       dispensingServo.write(SERVOHIGH);
       delay(500);
       dispensingServo.write(SERVOLOW);
+      
     break;
-    case (4):
-      //4  Clear Motion Triggered  : VOID
+    case (CLEAR_MOTION):
+      
       motionIsTriggered = false;
-      break;
-    case (7):
-    //7 Acknowledge returns a 7 to over UART.  This is intended to be used to confirm that the UART connection is open and reading correctly
+      
+    break;
+    case (ACK):
+    //Acknowledge returns a 7 to over UART.  This is intended to be used to confirm that the UART connection is open and reading correctly
+    
       Serial.print("7");
       Serial.flush();
+      
     break;
     default:
-      //invalid command becomes a NOP
+      //invalid command or no operation (NOP)
     break;
   }
 
-  //Read value of motion sensor
-      if(digitalRead(PIRPIN)){
-        digitalWrite(13, HIGH);
-        motionIsTriggered = true;
-      }
-      else{
-        digitalWrite(13, LOW);
-      }
-
-  
+  // check for motion
+  if(digitalRead(PIRPIN)){
+    
+    motionIsTriggered = true;
+    
+  }
 
 }
